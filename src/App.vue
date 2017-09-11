a994a7ce3bde04f7e8e79ede6047d01317954d42
David Ky <justexit@gmail.com>
2017-09-11 16:59:24 -0700

    setup scss

--- src/App.vue
+++ src/App.vue
@@ -93,6 +93,7 @@ export default {
 </script>
 
 <style lang="scss">
+.about { background: blue; }
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
