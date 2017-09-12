01547b09f2fa52d074abc395fd9906572077ea2d
David Ky <justexit@gmail.com>
2017-09-12 14:04:39 -0700

    fixed font for build

--- build/vue-loader.conf.js
+++ build/vue-loader.conf.js
@@ -9,10 +9,11 @@ module.exports = {
       : config.dev.cssSourceMap,
     extract: isProduction
   }),
-  // load scss file globally
-  // loaders: {
-  //  scss: 'vue-style-loader!css-loader!sass-loader?data=@import "./src/assets/scss/_dashboard";'
-  // },
+  loaders: {
+    // load scss file globally
+    // scss: 'vue-style-loader!css-loader!sass-loader?data=@import "./src/assets/scss/_dashboard";'
+    scss: 'vue-style-loader!css-loader!sass-loader'
+  },
   transformToRequire: {
     video: 'src',
     source: 'src',
